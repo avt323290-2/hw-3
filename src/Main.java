fdb90032c9cd80a467b03efd4e7f9272cf6bff1b
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-09-13 19:40:06 +0300

    Внесли дополнение в решение

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,21 @@
 
 import Cars.Jeep;
 import Cars.Toyota;
-
+/**
+ * Класс Main представляет собой точку входа в приложение, где создаются экземпляры
+ * различных типов автомобилей и выполняются операции заправки.
+ */
 public class Main {
     public static void main(String[] args) {
 
-
+        // Создание экземпляра седана Toyota
         Toyota toyota = new Toyota("Camry", "blue", "sedan", "petrol", "auto", 3, 2.5);
+        // Создание экземпляра внедорожника Jeep
         Jeep kamaz = new Jeep("Grand Cherokee", "orange", "track", "diesel", "manual", 6, 6);
 
+        // Вызов метода заправки для седана Toyota
         toyota.refuel();
+        // Вызов метода заправки для внедорожника Jeep
         kamaz.refuel();
     }
 }
